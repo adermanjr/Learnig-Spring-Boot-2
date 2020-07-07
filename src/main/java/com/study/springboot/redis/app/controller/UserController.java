acbf3d5fb2445b11fb29c5a057e24a98362fe08f
adermanjr <aderman.jr@gmail.com>
2020-07-07 14:45:13 -0300

    @ResponseStatus(HttpStatus.CREATED) on Post

--- src/main/java/com/study/springboot/redis/app/controller/UserController.java
+++ src/main/java/com/study/springboot/redis/app/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,6 +40,7 @@ public class UserController {
     }
  
     @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
     void addUser(@RequestBody User user) {
         userService.add(user);
         System.out.println("Added user " + user);
